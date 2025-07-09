c51b1ce31140e8d987f31b78ccd592f1a3a17c3b
chiaochiaooo <45312913+chiaochiaoo@users.noreply.github.com>
2025-07-09 15:42:04 -0400

    push to manger

--- Manager.py
+++ Manager.py
@@ -3,8 +3,6 @@ import linecache
 import sys
 import os
 from datetime import datetime, timedelta
-import pandas as pd
-import numpy as np
 import tkinter as tk
 import traceback
 import socket
@@ -12,8 +10,6 @@ import time
 import requests
 import json
 import threading
-
-import xml.etree.ElementTree as ET
 
 from psutil import process_iter
 import psutil
@@ -29,16 +25,16 @@ class Manager:
 	def __init__(self,ui_root,ems):
 
 		self.root = ui_root 
-		self.ems_enabled = ems
+		self.EMS_ADDRESS = ems
 
 		# GLOBAL BOOLEAN #
 
 		self.system_connected = False 
 
 		self.SYSTEM_STATUS = tk.StringVar(value="")
-
 		self.USER = tk.StringVar(value='')
 		self.ENV = tk.StringVar(value='')
+
 
 		self.POSITION_COUNT = tk.IntVar(value=0)
 		self.ORDER_COUNT = tk.IntVar(value=0)
@@ -54,10 +50,11 @@ class Manager:
 		# CORE DATA # 
 
 		self.symbols ={}
+		self.algos = {}
 
 
-		self.user = ""
-	
+
+
 		self.positions ={}
 		self.open_orders = {}
 
@@ -70,49 +67,60 @@ class Manager:
 
 
 		### WAIT FOR UI TO FULLY INSTANTIATE ###
-		while True:
-
-			try:
-				self.root.after(0, lambda: None)
-				break
-			except RuntimeError:
-				time.sleep(3)
+		# while True:
+		#
+		# 	try:
+		# 		self.root.after(0, lambda: None)
+		# 		break
+		# 	except RuntimeError:
+		# 		time.sleep(3)
 
 
 	### EMS PART ###
 
 	def get_connectivity(self):
-		env, user = self.get_env()
 
-		connected = True
-		if env==None:
-			connected=False
+		try:
+			r = f'http://{self.EMS_ADDRESS}:5000/connection'
+			response = requests.get(r,timeout=0.25)
+			data = response.json()
 
-		if connected != self.system_connected:
-			if connected:
+			success = data.get("ret", "")
+
+		except Exception as e:
+			#print(e)
+			success = False
+
+		if success != self.system_connected:
+
+			if success:
+				env,user = self.get_env()
 				self.USER.set(user)
 				self.ENV.set(env)
 			else:
 				self.USER.set('DISCONNECTED')
 				self.ENV.set('DISCONNECTED')
-			self.system_connected = connected
+			self.system_connected = success
 
 		return self.system_connected
 
 	def get_env(self):
 		try:
-			r = 'http://127.0.0.1:5000/getuser'
-			response = requests.get(r, timeout=0.25)
+			r = f'http://{self.EMS_ADDRESS}:5000/getuser'
+			response = requests.get(r,timeout=0.25)
 			data = response.json()
 
-			success = resp.get("ret", "").lower() == "true"
+			success = data.get("ret", "")
 
-			if sucess:
-				environment = data.get("Environment")
-				user = data.get("User")
+			#print(data)
+
+			if success:
+				environment = data.get("environment")
+				user = data.get("user")
 				return environment, user
-		except:
+		except Exception as e:
 			pass
+			#print(e)
 		return None, None
 
 	def inspection_loop(self):
@@ -138,6 +146,19 @@ class Manager:
 
 							
 			except Exception as e:
-				PrintException("Inspection error:")
+				PrintException("Inspection error:",e)
 
 			time.sleep(self.inspection_timer)
+
+
+EMS_ADDRESS = "127.0.0.1"
+EMS_ADDRESS = "10.29.10.137"
+
+tk.Tk()
+manager = Manager(None,EMS_ADDRESS)
+
+
+
+if m.get_connectivity():
+	print(m.USER.get(),m.ENV.get())
+
