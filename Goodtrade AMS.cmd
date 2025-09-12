563f6196c844384c0b8cde790d72affb80020eda
chiaochiaooo <45312913+chiaochiaoo@users.noreply.github.com>
2025-09-12 12:08:47 -0400

    Update Goodtrade AMS.cmd

--- Goodtrade AMS.cmd
+++ Goodtrade AMS.cmd
@@ -1,4 +1,4 @@
 git pull
 
-start "" cmd.exe /k "title Ppro EMS && python ems.py"
+start "" cmd.exe /c "title Ppro EMS && python ems.py"
 start "" cmd.exe /c "title GoodTrade AMS && python Manager.py"
