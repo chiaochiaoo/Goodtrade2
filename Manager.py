56b226854ccc3f148e64debcbed18656b13376f8
chiaochiaooo <45312913+chiaochiaoo@users.noreply.github.com>
2025-11-03 09:25:21 -0500

    Update Manager.py

--- Manager.py
+++ Manager.py
@@ -1315,7 +1315,7 @@ EMS_ADDRESS = "127.0.0.1"
 #EMS_ADDRESS = "10.29.10.137"
 
 root = tb.Window(themename="flatly") # Start with a light theme
-root.title("GoodTrade AMS 10-22")
+root.title("GoodTrade AMS 11-03")
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
