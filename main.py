80f3d7e6b51c613980d0ee484a9e9b7ea97d208d
chiaochiaooo <45312913+chiaochiaoo@users.noreply.github.com>
2026-07-17 15:04:57 -0400

    17

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     force_close_port(4440)
 
     root = tb.Window(themename="flatly")
-    root.title("GoodTrade AMS 07-15")
+    root.title("GoodTrade AMS 07-17")
 
     root.geometry("1870x1280")
 
